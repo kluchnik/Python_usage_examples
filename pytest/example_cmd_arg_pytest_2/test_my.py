0f0b99492bcd18895b98ef56fa2be2b7c70047de
Kluchnik <44801636+kluchnik@users.noreply.github.com>
2023-06-07 11:53:24 +0300

    Create /pytest/example_cmd_arg_pytest_2/test_my.py

---
+++ pytest/example_cmd_arg_pytest_2/test_my.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_1(param_1, param_2, param_3):
+  ''' Тестовая функция '''
+  print('\nstart test function')
+  print(param_1)
+  print(param_2)
+  print(param_3)
+  print('stop test function')
+  assert True
